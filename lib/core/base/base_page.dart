e01c72f6176012d1d8029f371324899e54f0ea92
Robby Surya Pratama <robby@skinmystery.com>
2022-12-26 09:41:06 +0700

    feat: add error handling mixin

--- lib/core/base/base_page.dart
+++ lib/core/base/base_page.dart
@@ -1,11 +1,12 @@
+import 'package:eczema/core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 abstract class BasePage extends ConsumerStatefulWidget {
   const BasePage({super.key});
-
 }
 
 abstract class BaseState<Page extends BasePage> extends ConsumerState<Page> {
   String screenName();
+  Widget errorWidget(Failure failure);
 }
--- lib/core/mixins/error_handling_mixin.dart
+++ lib/core/mixins/error_handling_mixin.dart
@@ -2,6 +2,7 @@ import 'package:eczema/core/core.dart';
 import 'package:flutter/material.dart';
 
 mixin ErrorHandlingErrorMixin<Page extends BasePage> on BaseState<Page> {
+  @override
   Widget errorWidget(Failure failure) {
     if (failure is ServerFailure) {
       return Container();
